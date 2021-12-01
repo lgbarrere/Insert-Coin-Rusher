d90dbf16697ca6af6fe4bc39df1a94bdac8353b9
lgbarrere <louis.gabriel.barrere@gmail.com>
2021-12-01 21:45:03 +0100

    Camera rotation fix
    > Fixed bug that caused the camera to lock sometimes
    > CameraMovement script cleaned and optimized

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,43 +2,23 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Transform cameraTransform;
     public float cameraRotationSpeed = 100f;
-    float maxDegreRotation = 315;
-    float minDegreRotation = 45;
-    float initialRotationDegre = 0;
-    //public float widthRatio = 0.1f;
-    private void Start()
-    {
-        initialRotationDegre = cameraTransform.eulerAngles.y;
-    }
+    public const float maxAngle = 45f;
+    public const float minAngle = -45f;
+    private float yRotation = 0;
 
     // Update is called once per frame
     void Update()
     {
-        //Input.mousePosition.x < Screen.width / 2 - Screen.width * widthRatio
-        if (Input.GetKey("left") || Input.GetKey("a"))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            
-            if ((cameraTransform.rotation.eulerAngles.y -cameraRotationSpeed*Time.deltaTime >= maxDegreRotation && cameraTransform.rotation.eulerAngles.y <= 365) ^ (cameraTransform.eulerAngles.y <= minDegreRotation && cameraTransform.eulerAngles.y >= 0))
-            {
-                cameraTransform.Rotate(new Vector3(0, -cameraRotationSpeed * Time.deltaTime, 0));
-            }
+            yRotation -= cameraRotationSpeed * Time.deltaTime;
         }
-        //Input.mousePosition.x > Screen.width / 2 + Screen.width * widthRatio
-        if (Input.GetKey("right") || Input.GetKey("e"))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.E))
         {
-            if ((cameraTransform.eulerAngles.y +cameraRotationSpeed*Time.deltaTime <= minDegreRotation && cameraTransform.eulerAngles.y >= 0) ^ (cameraTransform.eulerAngles.y >= maxDegreRotation && cameraTransform.eulerAngles.y <= 365))
-            {
-                cameraTransform.Rotate(new Vector3(0, cameraRotationSpeed * Time.deltaTime, 0));
-            }
-            else if (cameraTransform.eulerAngles.y > maxDegreRotation)
-            {
-                //cameraTransform.eulerAngles = new Vector3(0, initialRotationDegre + maxDegreRotation, 0);
-                cameraTransform.Rotate(new Vector3(0, cameraRotationSpeed * Time.deltaTime, 0));
-            }
+            yRotation += cameraRotationSpeed * Time.deltaTime;
         }
-
-
+        yRotation = Mathf.Clamp(yRotation, minAngle, maxAngle);
+        transform.eulerAngles = new Vector3(0.0f, yRotation, 0.0f);
     }
 }
