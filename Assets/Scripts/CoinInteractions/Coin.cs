5b921130f38d1b7d21c6b184155884e1dd62c91d
lgbarrere <louis.gabriel.barrere@gmail.com>
2022-01-16 21:42:22 +0100

    Coin moving to inventory feedback
    > When a fallen coin is clicked, we can clearly see it moving fast to the inventory
    > Few changes in the UI canvas to implement this feature
    * Note : If we click fast to the left and right fallen coins, the coin may not travel fast enough to reach the inventory (coin not added)

--- Assets/Scripts/CoinInteractions/Coin.cs
+++ Assets/Scripts/CoinInteractions/Coin.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Coin : MonoBehaviour
 {
     [SerializeField] Animator animator;
     [SerializeField] AudioSource audioSource;
     [SerializeField] GameManager gameManager;
+    public Image coinFeedback;
+    public RectTransform coinMovingPos;
+    public RectTransform inventoryTransform;
+    public RectTransform CanvasRect;
+    public float coinMovingSpeed = 3000f;
 
-    // Start is called before the first frame update
     void Start()
     {
         if (animator == null) animator = GetComponent<Animator>();
         if (audioSource == null) audioSource = GetComponentInChildren<AudioSource>();
-
+        
         NextCoin();
+    }
+
+    void Update()
+    {
+        if (coinFeedback.enabled)
+        {
+            MoveCoinToInventory();
+        }
     }
 
     // Update is called once per frame
@@ -34,14 +47,40 @@ public class Coin : MonoBehaviour
     {
         audioSource.Play();
     }
-    
+
+    void MoveCoinToInventory()
+    {
+        float step = coinMovingSpeed * Time.deltaTime;
+        coinMovingPos.position = Vector3.MoveTowards(coinMovingPos.position, inventoryTransform.position, step);
+
+        // If the coin approximatively reached the inventory, reset the moving coin
+        if (Vector3.Distance(coinMovingPos.position, inventoryTransform.position) < 0.001f)
+        {
+            ResetMovingCoin();
+            gameManager.AddCoin();
+        }
+    }
+
+    void ResetMovingCoin()
+    {
+        coinMovingPos.position = Vector3.zero;
+        coinFeedback.enabled = false;
+    }
+
+    void TriggerCoinMoving()
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(CanvasRect, Input.mousePosition, null, out Vector2 anchoredPos);
+        coinMovingPos.anchoredPosition = anchoredPos;
+
+        coinFeedback.enabled = true;
+    }
+
     void OnMouseDown()
     {
         if (!gameManager.MaxCoinReached())
         {
-            gameManager.AddCoin();
             animator.SetTrigger("takeCoin");
-
+            TriggerCoinMoving();
             NextCoin();
         }
     }
